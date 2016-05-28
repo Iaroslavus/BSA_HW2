036f943d037b449eccc41607357caf4884700f34
Yaroslav Yanko <Yaroslav Yanko>
2016-05-28 23:07:19 +0300

    deleted

--- BSA_HW/Marvel/GeneratorOfTriangleNumbers.php
+++
@@ -1,9 +0,0 @@
-<?php   
-function genTriangleNumber($yild = 15) {
-    for ($i = 0; $i < $yild; $i++) {
-        yield $i / 2 * ($i + 1);
-    }
-}
-foreach (genTriangleNumber() as $value) {
-     echo '<br>' . $value;
-}
--- BSA_HW/vendor/bin/pixeler.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-setlocal DISABLEDELAYEDEXPANSION
-SET BIN_TARGET=%~dp0/../lastguest/pixeler/bin/pixeler
-php "%BIN_TARGET%" %*
